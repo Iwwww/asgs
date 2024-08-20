fd9c6de434452c819f04591a1804530bb0d2d68b
Iwwww <mike.yolgin@gmail.com>
2024-08-20 21:58:25 +0300

    Update Toast Notification in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@radix-ui/react-tooltip";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-    <Toaster />
+    <TooltipProvider>
+      <App />
+      <Toaster />
+    </TooltipProvider>
   </React.StrictMode>,
 );
