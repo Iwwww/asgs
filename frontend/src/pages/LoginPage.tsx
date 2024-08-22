bb8910e2c34abd2e0f58c7f5433ac748885550df
Iwwww <mike.yolgin@gmail.com>
2024-08-22 20:48:20 +0300

    Fix styles

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -100,7 +100,7 @@ export function LoginForm() {
 
 export default function LoginPage() {
   return (
-    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
+    <div className="w-full min-h-screen lg:grid lg:min-h-screen lg:grid-cols-2">
       <div className="flex items-center justify-center py-12 mb-20">
         <div className="mx-auto grid w-[350px] gap-6">
           <div className="grid gap-2 text-center">
@@ -118,7 +118,7 @@ export default function LoginPage() {
           alt="Image"
           width="1920"
           height="1080"
-          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale object-contain"
+          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
       </div>
     </div>
