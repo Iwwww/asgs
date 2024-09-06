8bb8bb1f5d3390d1a300cfe585144955a0654e4f
Iwwww <mike.yolgin@gmail.com>
2024-09-06 17:27:52 +0300

    Update pagination settings

--- backend/asgs/settings.py
+++ backend/asgs/settings.py
@@ -184,7 +184,7 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,
+    "PAGE_SIZE": 1000,
 }
 
 AUTH_USER_MODEL = "core.ExtendedUser"
